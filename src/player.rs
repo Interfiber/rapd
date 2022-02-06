183ffd5100bad6e78566f14e1a1a2bd1cafc438d
Interfiber <webmaster@interfiber.dev>
2022-02-06 18:36:26 -0500

    more log messages

--- src/player.rs
+++ src/player.rs
@@ -5,7 +5,9 @@ use crate::requests::AudioPlayRequest;
 pub fn play_audio_file(file: &str) {
     let sl = Soloud::default().unwrap();
     let mut wav = audio::Wav::default();
-    wav.load(&std::path::Path::new(file)).expect("Failed to load audio data into memor      y");
+    info!("Loading audio data into memory");
+    wav.load(&std::path::Path::new(file)).expect("Failed to load audio data into memory");
+    info!("Starting audio playback");
     sl.play(&wav);
     while sl.voice_count() > 0 {
         if Path::new("rapd.stop_player_thread").exists() {
@@ -15,9 +17,17 @@ pub fn play_audio_file(file: &str) {
         }
         std::thread::sleep(std::time::Duration::from_millis(100));
     }
+    info!("Audio playback completed for file: {}", file);
 }
 
 pub fn play_audio_from_request(request: AudioPlayRequest) {
     info!("Spawning audio thread...");    
-    Builder::new().name("player".to_string()).spawn(move || play_audio_file(&request.audio_file_path));
+    match Builder::new().name("player".to_string()).spawn(move || play_audio_file(&request.audio_file_path)){
+        Ok(_) => {
+            info!("Spawned audio thread");
+        },
+        Err(err) => {
+            error!("Failed to spawn audio thread: {}", err);
+        }
+    }
 }
