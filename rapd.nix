05f00b05a34e59d18d66d3620e71037f9c63bc97
Interfiber <webmaster@interfiber.dev>
2022-02-18 19:42:33 -0500

    update rapd.nix

--- rapd.nix
+++ rapd.nix
@@ -1,6 +1,6 @@
 # very basic rust nix config
 with import <nixpkgs> {};
 stdenv.mkDerivation {
-    name = "dev-environment"; # Probably put a more meaningful name here
+    name = "rapd-development"; # Probably put a more meaningful name here
     buildInputs = [ pkg-config cmake gcc rustup gnumake alsa-lib alsa-oss ];
 }
